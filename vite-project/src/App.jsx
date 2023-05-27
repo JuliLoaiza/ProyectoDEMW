f63f40aaadeddb70e962be3c0171443258abba9f
= <100366113+AnaMontanoM@users.noreply.github.com>
2023-05-27 18:23:56 -0500

    token jwt, cierta parte

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -8,43 +8,49 @@ import SignUp from "./components/signup_component";
 import UserDetails from "./components/userDetails";
 
 function App() {
-    return (
-        < Router >
-            <div className="App">
-                <nav className="navbar navbar-expand-lg navbar-light fixed-top">
-                    <div className="container">
-                        <Link className="navbar-brand" to={'/sign-in'}>
-                            DEMW
-                        </Link>
-                        <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                            <ul className="navbar-nav ml-auto">
-                                <li className="nav-item">
-                                    <Link className="nav-link" to={'/sign-in'}>
-                                        Log In
-                                    </Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to={'/sign-up'}>
-                                        Sign up
-                                    </Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </nav>
-                <div className="auth-wrapper">
-                    <div className="auth-inner">
-                        <Routes>
-                            <Route exact path="/" element={<Login />} />
-                            <Route path="/sign-in" element={<Login />} />
-                            <Route path="/sign-up" element={<SignUp />} />
-                            <Route path="/userDetails" element={<UserDetails />} />
-                        </Routes>
-                    </div>
-                </div>
-            </div>
-        </Router >
-    );
+	return (
+		<Router>
+			<div className="App">
+				<nav className="navbar navbar-expand-lg navbar-light fixed-top">
+					<div className="container">
+						<Link className="navbar-brand" to={"/sign-in"}>
+							DEMW
+						</Link>
+						<div
+							className="collapse navbar-collapse"
+							id="navbarTogglerDemo02"
+						>
+							<ul className="navbar-nav ml-auto">
+								<li className="nav-item">
+									<Link className="nav-link" to={"/sign-in"}>
+										Log In
+									</Link>
+								</li>
+								<li className="nav-item">
+									<Link className="nav-link" to={"/sign-up"}>
+										Sign up
+									</Link>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+				<div className="auth-wrapper">
+					<div className="auth-inner">
+						<Routes>
+							<Route exact path="/" element={<Login />} />
+							<Route path="/sign-in" element={<Login />} />
+							<Route path="/sign-up" element={<SignUp />} />
+							<Route
+								path="/userDetails"
+								element={<UserDetails />}
+							/>
+						</Routes>
+					</div>
+				</div>
+			</div>
+		</Router>
+	);
 }
 
-export default App
+export default App;
